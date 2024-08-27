d3298f4436bdcf6203dee707691819c658c9892b
Paulina Flores <paufr.2822@gmail.com>
2024-08-27 01:49:35 -0700

    Update homepage with chat

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useRef } from "react";
+import Chat from "./components/Chat";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
 export default function Home() {
+  const chatInputRef = useRef(null);
+
+  const handleAskClick = () => {
+    chatInputRef.current?.focus();
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      style={{
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+      }}
+    >
+      <Container sx={{}}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: { xs: "center", lg: "left" },
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <Typography
+              variant="h1"
+              gutterBottom
+              sx={{
+                color: "#432818",
+                fontSize: "3rem",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Welcome to CoffeeCurator
+            </Typography>
+            <Typography
+              variant="h3"
+              sx={{
+                color: "#7F5539",
+                fontSize: "1.5rem",
+                textAlign: "center",
+              }}
+            >
+              Your AI advisor for that cup of coffee!
+            </Typography>
+            <Button
+              variant="contained"
+              onClick={handleAskClick}
+              sx={{
+                display: "flex",
+                width: "fit-content",
+                alignItems: "center",
+                mt: 4,
+                py: 2,
+                backgroundColor: "#432818 !important",
+                mx: "auto",
+              }}
+            >
+              Ask our AI <ArrowRightAltIcon sx={{ ml: 1 }} />
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Chat inputRef={chatInputRef} />
+          </Grid>
+        </Grid>
+      </Container>
     </main>
   );
 }
